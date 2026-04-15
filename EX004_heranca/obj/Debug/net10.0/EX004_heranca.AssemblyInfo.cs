86b3fe57cb25235621448e341f176c485e6b882f
Lucas Roldão Cardoso <lucasroldao2802@gmail.com>
2026-04-14 22:30:48 -0300

    feat: Praticando C#

--- EX004_heranca/obj/Debug/net10.0/EX004_heranca.AssemblyInfo.cs
+++ EX004_heranca/obj/Debug/net10.0/EX004_heranca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EX004_heranca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59dcf39c6fc665caf469ffa438442aeb554d282c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de6bce85c3e9009a678740951b0fb2ff4bb58dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EX004_heranca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EX004_heranca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
